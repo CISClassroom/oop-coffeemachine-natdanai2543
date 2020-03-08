6308819e4ecfb070b90b4e97f10afc954953e7c7
ณัฐดนัย <natdanai.nt@kkumail.com>
2020-03-08 19:49:46 +0700

    งานคอฟฟี่

---
+++ src/com/company/Americano.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Americano {
+    public static void main(String[] args) {
+        CoffeeMachine coffeeMachine = new CoffeeMachine();
+        coffeeMachine.MakeAmericano(6);
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static class main {
+
+        public static void main(String[] arge) {
+            CoffeeMachine coffeeMachine = new CoffeeMachine();
+            coffeeMachine.MakeCoffee(6);
+        }
+    }
+}
+
